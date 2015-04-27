6b17f915a14454d3494a07eace163cb45a6d5f61
Jan Godziatkowski <ph.godzio@gmail.com>
2015-04-27 12:16:25 +0200

    Odkomentowanie i poprawienie Testów

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -1,38 +1,39 @@
 package edu.iis.mto.integrationtest.repository;
 
+import edu.iis.mto.integrationtest.model.Person;
+import java.util.List;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+
 import static edu.iis.mto.integrationtest.repository.PersonBuilder.person;
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
-
-import org.junit.Test;
-
-import edu.iis.mto.integrationtest.model.Person;
-
-
 public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
-	
-	private PersonRepository personRepository;
+    @Autowired
+    private PersonRepository personRepository;
 
-	@Test
-	public void testCanAccessDbAndGetTestData() {
-//		List<Person> foundTestPersons = personRepository.findAll();
-//		assertEquals(2, foundTestPersons.size());
-	}
+    @Test
+    public void testCanAccessDbAndGetTestData() {
+        List<Person> foundTestPersons = personRepository.findAll();
+        assertEquals( 2, foundTestPersons.size() );
+    }
 
-	@Test
-	public void testSaveNewPersonAndCheckIsPersisted() {
-//		long count = personRepository.count();
-//		personRepository.save(a(person().withId(count + 1)
-//				.withFirstName("Roberto").withLastName("Mancini")));
-//		assertEquals(count + 1, personRepository.count());
-//		assertEquals("Mancini", personRepository.findOne(count + 1)
-//				.getLastName());
-	}
+    @DirtiesContext
+    @Test
+    public void testSaveNewPersonAndCheckIsPersisted() {
+        long count = personRepository.count();
+        personRepository.save( a( person().withId( count + 1 )
+                .withFirstName( "Roberto" ).withLastName( "Mancini" ) ) );
+        assertEquals( count + 1, personRepository.count() );
+        assertEquals( "Mancini", personRepository.findOne( count + 1 )
+                .getLastName() );
+    }
 
-	private Person a(PersonBuilder builder) {
-		return builder.build();
-	}
+
+    private Person a( PersonBuilder builder ) {
+        return builder.build();
+    }
 
 }
