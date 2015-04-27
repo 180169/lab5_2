16e37356fe69250368766a33ba310817b9fac674
Jan Godziatkowski <ph.godzio@gmail.com>
2015-04-27 18:24:38 +0200

    Dodanie metody do interfejsu

--- src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
+++ src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
@@ -1,8 +1,10 @@
 package edu.iis.mto.integrationtest.repository;
 
 import edu.iis.mto.integrationtest.model.Person;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    public List<Person> findByFirstNameLike( String firstName );
 }
