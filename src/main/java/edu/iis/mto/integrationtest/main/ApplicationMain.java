6a53a4044613aca790ab95070a6e7dbde1069519
Jan Godziatkowski <ph.godzio@gmail.com>
2015-04-27 11:20:18 +0200

    Zmiana poziomu dostepu do Loggera

--- src/main/java/edu/iis/mto/integrationtest/main/ApplicationMain.java
+++ src/main/java/edu/iis/mto/integrationtest/main/ApplicationMain.java
@@ -1,22 +1,20 @@
 package edu.iis.mto.integrationtest.main;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import edu.iis.mto.integrationtest.utils.Mode;
 import edu.iis.mto.integrationtest.utils.ModeUtils;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApplicationMain {
 
 //    private static ApplicationContext applicationContext;
 //
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMain.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger( ApplicationMain.class );
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         /* Ustawienie trybu dzia³ania aplikacji */
-        ModeUtils.setMode(Mode.DEV);
-        LOGGER.debug("Application mode set to ", Mode.DEV);
+        ModeUtils.setMode( Mode.DEV );
+        LOGGER.debug( "Application mode set to ", Mode.DEV );
 //
 //        /* Konfiguracja kontekstu Spring i pobranie z kontenera bean'a reprezentujacego repozytorium danych*/
 //        applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
