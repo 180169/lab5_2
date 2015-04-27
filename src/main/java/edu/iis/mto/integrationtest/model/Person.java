baff04ca2befd253ba881a3d67ec0e2c0f4028ee
Jan Godziatkowski <ph.godzio@gmail.com>
2015-04-27 11:20:41 +0200

    Zmiana logiki w klasie Person aby wykorzystywala JPA

--- src/main/java/edu/iis/mto/integrationtest/model/Person.java
+++ src/main/java/edu/iis/mto/integrationtest/model/Person.java
@@ -1,38 +1,47 @@
 package edu.iis.mto.integrationtest.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "person" )
 public class Person {
 
-	private Long id;
+    @Id
+    @Column( unique = true, nullable = false )
+    private Long id;
+    @Column( name = "first_name" )
+    private String firstName;
+    @Column( name = "last_name" )
+    private String lastName;
 
-	private String firstName;
+    public Long getId() {
+        return id;
+    }
 
-	private String lastName;
+    public void setId( Long id ) {
+        this.id = id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setFirstName( String firstName ) {
+        this.firstName = firstName;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setLastName( String lastName ) {
+        this.lastName = lastName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Person() {
-	}
+    public Person() {
+    }
 
 }
